d5e34afd03a8efa7ff27d65fd1582e7ef31c45f7
swapnilbarahate <barahateswapnil2017@gmail.com>
2023-07-29 09:06:41 +0530

    decimal number convet to the octal solution

---
+++ decimal_to_octal.js
@@ -0,0 +1,16 @@
+const prompt = require('prompt-sync')({ sigint: true });
+let decimal = prompt("Enter the decimal number: ");
+decimal = Number(decimal);
+function decimalToOctal(decimal){
+    let ans=0,mul=1,rem;
+    while(decimal>0)
+    {
+        rem = decimal%8;
+        ans = ans + rem * mul;
+        mul = mul*10;
+        decimal = parseInt(decimal/8);//ignore after decimal place value
+    }
+    return ans;
+}
+let octal = decimalToOctal(decimal);
+console.log(octal);
